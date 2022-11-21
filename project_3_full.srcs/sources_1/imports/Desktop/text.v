ec608885e9a7ddbb26d1643e7b22b9b89c8eaa1e
Kiriki-liszt <kirikiaris@gmail.com>
2022-11-21 16:54:57 +0900

    cnt dwn try

--- project_3_full.srcs/sources_1/imports/Desktop/text.v
+++ project_3_full.srcs/sources_1/imports/Desktop/text.v
@@ -91,18 +91,41 @@ module clk_div_1k(clk, reset, clk_1k);
 			end
 		end
 	end
-	
+endmodule
+
+module update(
+	input sub_in;
+	input [3:0] semi_in;
+	input able;
+	output reg sub_out;
+	output reg [3:0] semi_out;
+);
+
+	if (sub_in) begin
+		if ((semi_in == 4'b0) && able) begin
+			semi_out = 4'b9;
+			sub_out = 1'b1;
+		end
+		else begin 
+			semi_out = semi_in - 4'b1;
+			sub_out = 1'b0;
+		end
+	end
 endmodule
 
 
 `timescale 1ns/100ps
-module tb_clk_div(clk_pin, rst_pin, mid_pin, seg, digit);
-
-	input  				clk_pin;
-	input 				rst_pin;
-	input				mid_pin;
-	output reg [7:0]	digit;
-	output reg [7:0]	seg;
+module tb_clk_div(
+	input  				clk_pin,
+	input 				rst_pin,
+	input				mid_pin,
+	input				left_pin,
+	input				right_pin,
+	input				up_pin,
+	input				down_pin,
+	output reg [7:0]	digit,
+	output reg [7:0]	seg
+);
 
 	wire				clk_100;
 	wire				clk_1k;
@@ -115,16 +138,30 @@ module tb_clk_div(clk_pin, rst_pin, mid_pin, seg, digit);
 	reg [3:0] 			semi[7:0];
 	reg [3:0] 			bcd;
 	reg [7:0]			selected;
+	reg [7:0]			able;
+	reg [7:0] 			sub;
 
 	wire				clk_i;
 	wire				rst_i;
 	wire				rst_clk;
 	wire				mid_i;
 	wire				mid_clk;
-		
-	IBUF    IBUF_rst_i0    (.I (rst_pin),   .O (rst_i));
-	IBUF    IBUF_btn_i0    (.I (mid_pin),   .O (mid_i));
-	IBUF    IBUF_clk_i0    (.I (clk_pin),   .O (clk_i));
+	wire				left_i;
+	wire				left_clk;
+	wire				right_i;
+	wire				right_clk;
+	wire				up_i;
+	wire				up_clk;
+	wire				down_i;
+	wire				down_clk;
+	
+	IBUF	IBUF_clk_i0		(.I (clk_pin),		.O (clk_i));
+	IBUF	IBUF_rst_i0		(.I (rst_pin),		.O (rst_i));
+	IBUF	IBUF_mid_i0		(.I (mid_pin),		.O (mid_i));
+	IBUF	IBUF_left_i0	(.I (left_pin),		.O (left_i));
+	IBUF	IBUF_right_i0	(.I (right_pin),	.O (right_i));
+	IBUF	IBUF_up_i0		(.I (up_pin),		.O (up_i));
+	IBUF	IBUF_down_i0	(.I (down_pin),		.O (down_i));
 
 	meta_harden meta_harden_rst_i0 (
 		.clk_dst      (clk_i),
@@ -134,12 +171,41 @@ module tb_clk_div(clk_pin, rst_pin, mid_pin, seg, digit);
 	);
 
 	// And the button input
-	meta_harden meta_harden_btn_i0 (
+	meta_harden meta_harden_mid_i0 (
 		.clk_dst      (clk_i),
 		.rst_dst      (rst_clk),
 		.signal_src   (mid_i),
 		.signal_dst   (mid_clk)
 	);
+
+	meta_harden meta_harden_left_i0 (
+		.clk_dst      (clk_i),
+		.rst_dst      (rst_clk),
+		.signal_src   (left_i),
+		.signal_dst   (left_clk)
+	);
+
+	meta_harden meta_harden_right_i0 (
+		.clk_dst      (clk_i),
+		.rst_dst      (rst_clk),
+		.signal_src   (right_i),
+		.signal_dst   (right_clk)
+	);
+
+	meta_harden meta_harden_up_i0 (
+		.clk_dst      (clk_i),
+		.rst_dst      (rst_clk),
+		.signal_src   (up_i),
+		.signal_dst   (up_clk)
+	);
+
+	meta_harden meta_harden_down_i0 (
+		.clk_dst      (clk_i),
+		.rst_dst      (rst_clk),
+		.signal_src   (down_i),
+		.signal_dst   (downs_clk)
+	);
+
 		
 	clk_div_100	clk100	(clk_i, reset, clk_100);
 	clk_div_1k	clk1k	(clk_i, reset, clk_1k);
@@ -148,18 +214,28 @@ module tb_clk_div(clk_pin, rst_pin, mid_pin, seg, digit);
 	initial begin
 	    digit = 8'b1000_0000;
 		selected = 8'b1000_0000;
+		able = 8'b1111_1111;
 		pause = 1'b0;
 		blink = 1'b0;
-		semi[0] = 4'd0;
-		semi[1] = 4'd0;
-		semi[2] = 4'd0;
-		semi[3] = 4'd0;
-		semi[4] = 4'd0;
-		semi[5] = 4'd0;
-		semi[6] = 4'd0;
-		semi[7] = 4'd0;
+		semi[0] = 4'd9;
+		semi[1] = 4'd9;
+		semi[2] = 4'd5;
+		semi[3] = 4'd9;
+		semi[4] = 4'd5;
+		semi[5] = 4'd9;
+		semi[6] = 4'd9;
+		semi[7] = 4'd9;
+		clk_sim = 1'b0;
         #5820000	reset = 0;
 		#112		reset = 1;
+	end
+
+	always @ (negedge mid_clk) begin
+		pause = ~pause;
+	end
+
+	always @ (posedge clk_1) begin
+		blink = ~blink;
 	end
 
 	always @ (posedge clk_100) begin
@@ -174,49 +250,99 @@ module tb_clk_div(clk_pin, rst_pin, mid_pin, seg, digit);
 			semi[7] = 4'd0;
 		end
 
+		if (semi[7])			able[6] = 1'b1;
+		else					able[6] = 1'b0;
+		if (able[6] || semi[6])	able[5] = 1'b1;
+		else					able[5] = 1'b0;
+		if (able[5] || semi[5])	able[4] = 1'b1;
+		else					able[4] = 1'b0;
+		if (able[4] || semi[4])	able[3] = 1'b1;
+		else					able[3] = 1'b0;
+		if (able[3] || semi[3])	able[2] = 1'b1;
+		else					able[2] = 1'b0;
+		if (able[2] || semi[2])	able[1] = 1'b1;
+		else					able[1] = 1'b0;
+	
 		if (pause == 1'b0) begin
-			semi[0] = semi[0] + 4'd1;
-			if (semi[0] == 4'd10) begin
-				semi[0] = 4'd0;
-				semi[1] = semi[1] + 4'd1;
+			if (semi[0] == 4'd0) begin
+				if (able[0]) begin
+					semi[0] = 4'b9;
+					sub[1] = 1'b1;
+				end
 			end
-			if (semi[1] == 4'd10) begin 
-				semi[1] = 4'd0;
-				semi[2] = semi[2] + 4'd1;
-			end 
-			if (semi[2] == 4'd10) begin 
-				semi[2] = 4'd0;
-				semi[3] = semi[3] + 4'd1;
-			end 
-			if (semi[3] == 4'd6) begin 
-				semi[3] = 4'd0;
-				semi[4] = semi[4] + 4'd1;
-			end 
-			if (semi[4] == 4'd10) begin
-				semi[4] = 4'd0;
-				semi[5] = semi[5] + 4'd1;
+			else begin 
+				semi[0] = semi[0] - 4'b1;
 			end
-			if (semi[5] == 4'd6) begin 
-				semi[5] = 4'd0;
-				semi[6] = semi[6] + 4'd1;
-			end 
-			if (semi[6] == 4'd10) begin 
-				semi[6] = 4'd0;
-				semi[7] = semi[7] + 4'd1;
-			end 
-			if (semi[7] == 4'd10) begin 
-				semi[7] = 4'd0;
-			end 
+			if (sub[1]) begin
+				if ((semi[1] == 4'b0) && able[1]) begin
+					semi[1] = 4'b9;
+					sub[2] = 1'b1;
+				end
+				else begin 
+					semi[1] = semi[1] - 4'b1;
+					sub[2] = 1'b0;
+				end
+			end
+			if (sub[2]) begin
+				if ((semi[2] == 4'b0) && able[2]) begin
+					semi[2] = 4'b9;
+					sub[3] = 1'b1;
+				end
+				else begin 
+					semi[2] = semi[2] - 4'b1;
+					sub[3] = 1'b0;
+				end
+			end
+			if (sub[3]) begin
+				if ((semi[3] == 4'b0) && able[3]) begin
+					semi[3] = 4'b9;
+					sub[4] = 1'b1;
+				end
+				else begin 
+					semi[3] = semi[3] - 4'b1;
+					sub[4] = 1'b0;
+				end
+			end
+			if (sub[4]) begin
+				if ((semi[4] == 4'b0) && able[4]) begin
+					semi[4] = 4'b9;
+					sub[5] = 1'b1;
+				end
+				else begin 
+					semi[4] = semi[4] - 4'b1;
+					sub[5] = 1'b0;
+				end
+			end
+			if (sub[5]) begin
+				if ((semi[5] == 4'b0) && able[5]) begin
+					semi[5] = 4'b9;
+					sub[6] = 1'b1;
+				end
+				else begin 
+					semi[5] = semi[5] - 4'b1;
+					sub[6] = 1'b0;
+				end
+			end
+			if (sub[6]) begin
+				if ((semi[6] == 4'b0) && able[6]) begin
+					semi[6] = 4'b9;
+					sub[7] = 1'b1;
+				end
+				else begin 
+					semi[6] = semi[6] - 4'b1;
+					sub[7] = 1'b0;
+				end
+			end
+			if (sub[7]) begin
+				if ((semi[7] == 4'b0) && able[7]) begin
+					semi[7] = 4'b9;
+				end
+				else begin 
+					semi[7] = semi[7] - 4'b1;
+				end
+			end
 		end
 	end 
-
-	always @ (negedge mid_clk) begin
-		pause = ~pause;
-	end
-
-	always @ (posedge clk_1) begin
-		blink = ~blink;
-	end
 	
 	always @ (posedge clk_1k) begin
 
